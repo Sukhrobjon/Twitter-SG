eb1a43eae04b6a5f2ade2cca9813c6f60a8dd17b
sukhrobjongolibboev <sgolibbo@mail.ccsf.edu>
2016-02-15 16:13:44 -0800

    having problems about TweetsViewController

--- Twitter/Tweet.swift
+++ Twitter/Tweet.swift
@@ -9,24 +9,25 @@
 import UIKit
 
 class Tweet: NSObject {
-    
     var user: User?
     var text: String?
     var createdAtString: String?
     var createdAt: NSDate?
-
+    
+    
     init(dictionary: NSDictionary) {
         
         user = User(dictionary: dictionary["user"] as! NSDictionary)
         text = dictionary["text"] as? String
         createdAtString = dictionary["created_at"] as? String
         
-        var formatter = NSDateFormatter()
+        let formatter = NSDateFormatter()
         formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
         createdAt = formatter.dateFromString(createdAtString!)
+        
     }
     
-    class func tweetsWithArray(array: [NSDictionary]) ->[Tweet] {
+    class func tweetsWithArray(array: [NSDictionary]) -> [Tweet] {
         var tweets = [Tweet]()
         
         for dictionary in array {
@@ -35,4 +36,6 @@ class Tweet: NSObject {
         
         return tweets
     }
+    
+    
 }
--- Twitter/User.swift
+++ Twitter/User.swift
@@ -11,9 +11,11 @@ import UIKit
 
 var _currentUser: User?
 let currentUserKey = "kCurrentUserKey"
+let userDidLoginNotification = "userDidLoginNotification"
+let userDidLogoutNotification = "userDidLogoutNotification"
+
 
 class User: NSObject {
-
     var name: String?
     var screenname: String?
     var profileImageUrl: String?
@@ -22,35 +24,52 @@ class User: NSObject {
     
     init(dictionary: NSDictionary) {
         self.dictionary = dictionary
+        
         name = dictionary["name"] as? String
         screenname = dictionary["screen_name"] as? String
         profileImageUrl = dictionary["profile_image_url"] as? String
         tagline = dictionary["description"] as? String
- }
-
+    }
+    
+    func logout() {
+        User.currentUser = nil
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        
+        NSNotificationCenter.defaultCenter().postNotificationName(userDidLogoutNotification, object: nil)
+    }
     
     class var currentUser: User? {
         get {
-            return _currentUser
+        if _currentUser == nil {
+        //logged out or just boot up
+        let data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+        if data != nil {
+        let dictionary: NSDictionary?
+        do {
+        try dictionary = NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+        _currentUser = User(dictionary: dictionary!)
+    } catch {
+        print(error)
         }
-      
-            set(user) {
-                _currentUser = user
-                
-                if _currentUser != nil {
-                    do {
-                        let data = try NSJSONSerialization.dataWithJSONObject(user!.dictionary, options: NSJSONWritingOptions.PrettyPrinted)
-                        NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
-                    } catch(let error) {
-                        print(error)
-                        assert(false)
-                    }
-                } else {
-                     NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
         }
-                    NSUserDefaults.standardUserDefaults().synchronize()
+        }
+        return _currentUser
+        }
+        
+        
+        set(user) {
+            _currentUser = user
+            //User need to implement NSCoding; but, JSON also serialized by default
+            if let _ = _currentUser {
+                var data: NSData?
+                do {
+                    try data = NSJSONSerialization.dataWithJSONObject(user!.dictionary, options: .PrettyPrinted)
+                    NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+                } catch {
+                    print(error)
+                }
+            }
+        }
     }
-
-}
 }
 
